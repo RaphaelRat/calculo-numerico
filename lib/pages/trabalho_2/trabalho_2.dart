4c3989a07259814434d9d22159f25b3e18eca6e5
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-09 12:31:38 -0300

    add funcao field

--- lib/pages/trabalho_2/trabalho_2.dart
+++ lib/pages/trabalho_2/trabalho_2.dart
@@ -12,37 +12,80 @@ class Trabalho2 extends GetView<Trabalho2Controller> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         appBar: AppBar(title: const Text('T2 - Questão 4'), centerTitle: true),
-        body: Column(
-          children: [
-            Obx(
-              () => controller.integral.value.isEmpty
-                  // Image.asset('assets/integral.png'),
-                  ? Container()
-                  : Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+        body: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (overScroll) {
+            overScroll.disallowIndicator();
+            return false;
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Obx(
+                    () => Container(
+                      margin: const EdgeInsets.symmetric(vertical: 4),
                       width: double.infinity,
                       height: 80,
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12),
-                        color: Theme.of(context).primaryColor,
+                        color: Colors.grey.shade300,
                         border: Border.all(width: 1, color: Theme.of(context).focusColor),
                       ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Image.asset('assets/integral.png'), Text(controller.integral.value)],
-                      ),
+                      child: controller.integral.value.isEmpty
+                          ? const Center(
+                              child: Text(
+                                'Sua integral aqui',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          : Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset('assets/integral.png'),
+                                Text(
+                                  controller.integral.value,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
                     ),
+                  ),
+                  const SizedBox(height: 12),
+                  const Text('Entre com a integral:', style: TextStyle(fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 12),
+                  _myTextField(context, hintText: '2x+1/x', onChanged: (value) => controller.funcao.value = value),
+                ],
+              ),
             ),
-            TextField(
-              onChanged: (value) {
-                controller.integral.value = value;
-              },
-            )
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  TextField _myTextField(BuildContext context, {Function(String value)? onChanged, String? hintText}) {
+    return TextField(
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).focusColor, width: 3.0, style: BorderStyle.solid),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).focusColor, width: 1.0),
+        ),
+        hintText: hintText,
+      ),
+      onChanged: onChanged,
     );
   }
 }
--- lib/pages/trabalho_2/trabalho_2_controller.dart
+++ lib/pages/trabalho_2/trabalho_2_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
 class Trabalho2Controller extends GetxController {
-  final integral = ''.obs;
+  final integral = '2x+1/x'.obs;
+  final funcao = '2x+1/x'.obs;
 }
