94663c7f2c10403e3ed281a5afcace29a60a5c6f
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-08 22:14:00 -0300

    add Integral field

--- lib/pages/trabalho_2/trabalho_2.dart
+++ lib/pages/trabalho_2/trabalho_2.dart
@@ -10,6 +10,36 @@ class Trabalho2 extends GetView<Trabalho2Controller> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('T2 - Questão 4'), centerTitle: true),
+      body: Column(
+        children: [
+          Obx(
+            () => controller.integral.value.isEmpty
+                // Image.asset('assets/integral.png'),
+                ? Container()
+                : Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                    width: double.infinity,
+                    height: 80,
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Theme.of(context).primaryColor,
+                      border: Border.all(width: 1, color: Theme.of(context).focusColor),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Image.asset('assets/integral.png'), Text(controller.integral.value)],
+                    ),
+                  ),
+          ),
+          TextField(
+            onChanged: (value) {
+              controller.integral.value = value;
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- lib/pages/trabalho_2/trabalho_2_controller.dart
+++ lib/pages/trabalho_2/trabalho_2_controller.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
 
 class Trabalho2Controller extends GetxController {
-  int a = 5;
+  final integral = ''.obs;
 }
